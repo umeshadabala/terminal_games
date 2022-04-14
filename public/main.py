71398fe65cc597f3173e19890e418b767d5f727e
Umesh Adabala <84436237+umeshlab@users.noreply.github.com>
2022-04-14 12:28:01 +0530

    Update main.py

--- public/main.py
+++ public/main.py
@@ -15,5 +15,10 @@ I am not sure this will fit the theme or not
 but i gave a try.
 You can make everyday less ordinary by making a time table for your day
 and use this project every time your bored  by taking a break for few minutes.
+Commands
+1.help - To see the commands and few information about the project
+2.adventure - To play adventure game
+3.shooter - To play shooter game
+4.song - to play song game
 Thank You!
 ''')
